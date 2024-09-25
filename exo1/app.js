2135ca8da3824756d70164fdb8776e0626a62ebb
Benjamin Schacher <benjamin.schacher@laposte.net>
2024-09-25 16:00:36 +0200

    amelioration

--- exo1/app.js
+++ exo1/app.js
@@ -3,23 +3,27 @@ const creationForm = document.querySelector("#creationForm")
 const spiner = document.querySelector("#spinnerContainer")
 spiner.style.display = 'none'
 
+function makeMessages(Text, color) {
+    messages.textContent = Text
+    messages.style.color = color
+}
+
 creationForm.addEventListener('submit', async (event) => {
     event.preventDefault();
 
     spiner.style.display = 'block'
+    
     const formData = new FormData(creationForm)
     const email = formData.get("Email")
     const password = formData.get("Password")
     const confirmPassword = formData.get("confirmPassword")
 
     if (password.length < 8) {
-        messages.textContent = "Mot de passe trop court"
-        messages.style.color = "red"
+        makeMessages("Mot de passe trop court", "red")
         spiner.style.display = 'none'
         return
     } else if (password !== confirmPassword) {
-        messages.textContent = "Confirmation de mot de passe invalide"
-        messages.style.color = "red"
+        makeMessages("Confirmation de mot de passe invalide", "red")
         spiner.style.display = 'none'
         return
     }
@@ -42,19 +46,18 @@ creationForm.addEventListener('submit', async (event) => {
 
             if (!res.ok) {
                 const errorMessage = data['title']
-                messages.textContent = "Erreur : " + errorMessage
-                messages.style.color = "red"
+                messagesErreur = "Erreur : " + errorMessage
+                makeMessages(messagesErreur, "red")
                 spiner.style.display = 'none'
                 return
             }
 
-            messages.textContent = "Création réussite"
-            messages.style.color = "green"
+            makeMessages("Création réussite", "green")
             spiner.style.display = 'none'
             
         }   catch (err) {
-            messages.textContent = "Une erreur s'est produite : " + err;
-            messages.style.color = "red"
+            messagesCathErreur = "Une erreur s'est produite : " + err
+            makeMessages(messagesCathErreur, "red")
             spiner.style.display = 'none'
         }
     });
